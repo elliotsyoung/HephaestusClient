678f305c5d4e2b74672c0c5ebf886966c26d832a
Elliot Young <elliotsyoung@gmail.com>
2018-08-05 11:54:26 -0700

    first commit

---
+++ servo_controller.py
@@ -0,0 +1,14 @@
+from servosix import ServoSix
+import sys
+ss = ServoSix()
+
+try:
+    while True:
+        parsed_message = sys.stdin.readline().split(" ")
+        servo = int(parsed_message[0])
+        angle = int(parsed_message[1])
+        ss.set_servo(servo, angle)
+        sys.stdout.flush()
+finally:
+    print("End of Python process")
+    sys.stdout.flush()
