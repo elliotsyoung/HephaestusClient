6ed1131064d724c3b8a80a731fab9855a365ad58
Elliot Young <elliotsyoung@gmail.com>
2018-08-16 15:19:32 -0700

    one more modification to servo controller file

--- servo_controller.py
+++ servo_controller.py
@@ -4,10 +4,15 @@ ss = ServoSix()
 
 try:
     while True:
-        parsed_message = sys.stdin.readline().split(" ")
+        message_from_node = sys.stdin.readline()
+        parsed_message = message_from_node.split(" ")
         servo = int(parsed_message[0])
         angle = int(parsed_message[1])
         ss.set_servo(servo, angle)
+        print("turning head")
+        print(servo)
+        print(angle)
+        sys.stdout.flush()
 finally:
     print("End of Python process, cleaning up...")
     ss.cleanup()
