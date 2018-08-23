44f49c40ae80accaec74e15dbb96bd8d987425ba
Elliot Young <elliotsyoung@gmail.com>
2018-08-22 17:01:17 -0700

    changed some things around that might have been messing with the servo rotation

--- main.js
+++ main.js
@@ -146,9 +146,6 @@ ipcMain.on('detect face', (event, arg) =>
 })
 
 
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
 function rotate_head(angle)
 {
   console.log("Rotating head:", angle);
